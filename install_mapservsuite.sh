6418652cace45ca4eb7588cf721af0b949ae9a7e
root <root@precise32.(none)>
2013-03-26 15:09:38 +0000

    install mapserver suite bash

---
+++ install_mapservsuite.sh
@@ -0,0 +1,23 @@
+# Quick QA-VM setup
+# update encoding french canada and install ppa
+sudo localedef -i en_CA -f UTF-8 en_CA
+sudo update-locale
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+
+# install mapserver suite and tools (Postgresql/PostGIS, Mapserver Suite, Python Virtualenv, vim, zip)
+sudo apt-get -y install postgis postgresql-9.1 \
+      postgresql-server-dev-9.1 postgresql-contrib-9.1 \
+      postgis gdal-bin binutils libgeos-3.2.2 libgeos-c1 \
+      libgeos-dev libgdal1-dev libxml2 libxml2-dev \
+      libxml2-dev checkinstall proj libpq-dev git vim screen \
+      build-essential python-dev protobuf-compiler zip unzip\
+      libprotobuf-dev libtokyocabinet-dev python-psycopg2 \
+      python-virtualenv python-pip \
+      cgi-mapserver mapserver-bin libmapcache mapcache-cgi \
+      mapcache-tools libapache2-mod-mapcache tinyows
+
+# mapserver tricks
+sudo mkdir /tmp/ms_tmp
+sudo chown www-data:www-data /tmp/ms_tmp
